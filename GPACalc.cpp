607ba7d7a10d647f36f06e050a46ed473f5ee851
cjtorralba <cjtorralba@gmail.com>
2019-10-03 04:10:16 -0700

    another updated version

--- GPACalc.cpp
+++ GPACalc.cpp
@@ -8,12 +8,46 @@ using namespace std;
  */
 
 
+double charToDouble(char c)
+{
+        double returnVal; // value of the character
+
+        tolower(c);
+
+        switch(c) {
+                case 'a': returnVal = 4.0;
+                break;
+
+                case 'b': returnVal = 3.0;
+                break;
+
+		case 'c': returnVal = 2.0;
+		break;
+
+		case 'd': returnVal = 1.0;
+		break;
+
+		case 'f': returnVal = 0.0;
+		break;
+
+		default: break;
+                }
+
+	return returnVal;
+
+}
+
+
 int main()
 {
 
 	int totalClasses; // Total number of classes the user is taking
+
 	char letterGrade, plusOrMinus; // the users letter grade for specified class, and if the got a +/-
 
+	double totalGradePoints; // total amount of grade points, ex: a = 4.0, b- = 2.67
+
+	double avgGpa; // calculated by dividing totalGradePoints by totalClasses
 
 	cout << "How many classes are you taking?" << endl;
 
@@ -30,21 +64,19 @@ int main()
 			<< i << endl;
 
 			cin >> letterGrade;
+
 			plusOrMinus = cin.peek();
 
+//			cout << letterGrade << plusOrMinus;
 
-			cout << "You got a "
-			<< letterGrade
-			<< plusOrMinus
-			<< " in class number "
-			<< i << endl;
-
-
+			totalGradePoints += charToDouble(letterGrade);
 		}
 
+		
+	avgGpa = totalGradePoints / totalClasses;
 
-
-
-
+	cout << "Average GPA is: ";
+	cout.precision(3);
+	cout << avgGpa;
 
 }
