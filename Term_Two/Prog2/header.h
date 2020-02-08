4b02561e7ba48346e949b86de2de117d0bbc52f5
cjtorralba <cjtorralba@gmail.com>
2020-02-08 04:22:53 -0800

    backing up my stuff cause i accidentally deleted it all...

---
+++ Term_Two/Prog2/header.h
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+
+using namespace std;
+
+struct trip_section
+{
+	trip_section(); ~trip_section();
+	bool set_trip(char* name, char* traffic, char* notes, char* landmarks, int length);
+	char* name;
+	char* traffic;
+	char* notes;
+	char* landmarks;
+	int length;
+
+	trip_section* next;
+};
+
+struct node
+{
+	node* next;
+	trip_section* stack;
+};
+
+class return_trip
+{
+	private:
+		int top_index;
+		const int MAX = 5;
+		node* head;
+	public:
+		return_trip();  ~return_trip();
+		bool push(trip_section* node);
+		bool pop();
+		trip_section* peek();
+		bool display();
+		bool copy(trip_section* trip, trip_section* to_copy);
+
+};
+
+
+
+class travel
+{
+	private:
+		trip_section* rear;
+	public:
+		travel();       ~travel();
+		bool copy(trip_section* trip, trip_section* to_copy);
+		bool enqueue(trip_section* trip_section);
+		bool dequeue();
+		trip_section* peek();              bool display();
+		bool copy(trip_section* trip, trip_section* to_copy);
+
+};
+
+
+
---
+++ Term_Two/Prog2/main.cpp
@@ -0,0 +1,133 @@
+nclude "header.h"
+
+
+int main()
+{
+
+	travel circ1;
+	travel circ2;
+
+
+	return_trip stack1;
+	return_trip stack2;
+
+	trip_section trip;
+
+	char name[100];
+	char traffic[100];
+	char notes[100];
+	char landmarks[100];
+	int length;
+
+	bool exit = false;
+	char input;
+	int route;
+
+
+	cout << "Welcome to the trip planner..." << endl
+		<< "Please enter your first route, to do so, you will enter the name, \n"
+		<< "traffic status, any notes about the road, any landmarks, and the length (miles)" << endl;
+	do{
+
+		cout << "Enter the road name: " << endl;
+		cin.get(name, 100, '\n');       cin.ignore(100, '\n');
+
+		cout << "Enter traffic condition: " << endl;
+		cin.get(traffic, 100, '\n');    cin.ignore(100, '\n');
+
+		cout << "Enter notes: " << endl;
+		cin.get(notes, 100, '\n');      cin.ignore(100, '\n');
+
+		cout << "Enter any landmarks: " << endl;
+		cin.get(landmarks, 100, '\n');  cin.ignore(100, '\n');
+
+		cout << "Enter length: " << endl;
+		cin >> length;  cin.ignore(100, '\n');
+
+		trip.set_trip(name, traffic, notes, landmarks, length);
+
+		circ1.enqueue(&trip);
+		stack1.push(&trip);
+
+		cout << "Would you like to add another? <y,n>: ";
+		cin >> input;   cin.ignore(100, '\n');
+
+
+
+		f(input == 'n')
+			exit = true;
+	}while(!exit);
+
+	cout << "Please enter the information for the second trip" << endl;
+
+	exit = false;
+	input = '\0';
+
+	do{
+
+		cout << "Enter the road name: " << endl;
+		cin.get(name, 100, '\n');       cin.ignore(100, '\n');
+
+		cout << "Enter traffic condition: " << endl;
+		cin.get(traffic, 100, '\n');    cin.ignore(100, '\n');
+
+		cout << "Enter notes: " << endl;
+		cin.get(notes, 100, '\n');      cin.ignore(100, '\n');
+
+		cout << "Enter any landmarks: " << endl;
+		cin.get(landmarks, 100, '\n');  cin.ignore(100, '\n');
+
+		cout << "Enter length: " << endl;
+		cin >> length;  cin.ignore(100, '\n');
+
+		trip.set_trip(name, traffic, notes, landmarks, length);
+
+		circ2.enqueue(&trip);
+		stack2.push(&trip);
+
+		cout << "Would you like to add another? <y,n>: ";
+		cin >> input;   cin.ignore(100, '\n');
+
+		if(input == 'n')
+			exit = true;
+	}while(!exit);
+
+
+
+	cout << "This is the trip to your destination via route 1:" << endl;
+	circ1.display();
+
+	cout << "Hit enter to see the trip via route 2:" << endl;
+	cin; cin.ignore(100, '\n');
+	circ2.display();
+
+	cout << "Hit enter to see the trip back home via route 1:" << endl;
+	cin; cin.ignore(100, '\n');
+	stack1.display();
+
+	cout << "Hit enter to see the trip back home via route 2:" << endl;   cin; cin.ignore(100, '\n');
+	stack2.display();
+
+	cout << "Please choose a route <1,2>: ";
+	cin >> route;   cin.ignore(100, '\n');
+
+	if(route == 1)
+	{
+		circ2.~travel();
+		stack2.~return_trip();
+
+
+
+	}
+	else if(route == 2)
+	{
+		circ1.~travel();
+		stack1.~return_trip();
+
+	}
+
+
+
+	return 0;
+}
+
