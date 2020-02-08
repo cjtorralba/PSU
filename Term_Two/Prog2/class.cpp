3b4aaa794c2efbc54fbc2310fbd2115ca1fc7ad2
cjtorralba <cjtorralba@gmail.com>
2020-02-08 07:21:41 -0800

    almost done, testing for memory leaks

---
+++ Term_Two/Prog2/class.cpp
@@ -0,0 +1,370 @@
+#include "header.h"
+
+
+
+
+
+
+
+trip_section::trip_section()
+{
+	next = NULL;
+	name = NULL;
+	traffic = NULL;
+	notes = NULL;
+	landmarks = NULL;
+	length = 0;
+}
+
+
+trip_section::~trip_section()
+{
+	delete [] name;
+	delete [] traffic;
+	delete [] notes;
+	delete [] landmarks;
+	next = NULL;
+}
+
+bool trip_section::set_trip(char* nameAdd, char* trafficAdd, char* notesAdd, char* landmarksAdd, int lengthAdd)
+{
+	if(this->name != NULL)
+	{
+		delete [] name;
+		delete [] traffic;
+		delete [] notes;
+		delete [] landmarks;
+
+		name = NULL;
+		traffic = NULL;
+		notes = NULL;
+		landmarks = NULL;
+	}
+
+
+	name = new char[strlen(nameAdd) + 1];
+	traffic = new char[strlen(trafficAdd) + 1];
+	notes = new char[strlen(notesAdd) + 1];
+	landmarks = new char[strlen(landmarksAdd) + 1];
+	length = lengthAdd;
+	return true;	
+}
+
+
+
+travel::travel()
+{
+	rear = NULL;
+}
+
+
+travel::~travel()
+{
+	trip_section* cur;
+	trip_section* prev;
+
+	if(rear)
+	{
+		cur = rear->next;
+		rear->next = NULL;
+		while(cur->next != NULL)
+		{
+			prev = cur;
+			cur = cur->next;
+			delete prev;
+			prev->next = NULL;
+		}
+		delete rear;
+		}
+}
+
+
+
+bool travel::display()
+{
+	trip_section* temp;
+
+	if(!rear)
+		return NULL;
+	else
+	{
+		temp = rear;
+		do
+		{
+			temp = temp->next;
+			cout << "Name: " << temp->name << endl;
+		}while(temp != rear);
+		return true;
+	}
+	return false;
+}
+
+
+
+trip_section* peek()
+{
+	if(!rear)
+		return NULL;
+	else
+		return rear->next;
+	return NULL;
+}
+
+
+
+bool travel_dequeue()
+{
+	trip_section* cur;
+
+	if(!rear)
+		return false;
+	else
+	{
+		if(rear->next == rear)
+		{
+			delete rear;
+			rear = NULL;
+			return true;
+		}
+		else
+		{
+			for(cur = rear->next; cur->next != rear; cur = cur->next);
+			cur->next = rear->next;
+			delete rear;
+			rear = cur;
+			return true;
+		}
+	}
+	return false;
+}
+
+
+
+
+
+bool travel::enqueue(trip_section* trip)
+{
+
+	trip_section* temp;
+	if(rear = NULL)
+	{
+		rear = new trip_section;
+		rear->next = rear;
+
+		rear->name = new char[strlen(trip->name) + 1];
+		rear->traffic = new char[strlen(trip->traffic) + 1];
+		rear->notes = new char[strlen(trip->notes) + 1];
+		rear->landmarks = new char[strlen(trip->landmarks) + 1];
+
+		strcpy(rear->name, trip->name);
+		strcpy(rear->traffic, trip->traffic);
+		strcpy(rear->notes, trip->notes);
+		strcpy(rear->landmarks, trip->landmarks);
+		rear->length = trip->length;
+
+		return true;
+	}
+	else
+	{
+		temp = rear->next;
+		rear->next = new node;
+		rear = rear->next;
+		rear->next = temp;
+
+		rear->name = new char[strlen(trip->name) + 1];
+		rear->traffic = new char[strlen(trip->traffic) + 1];
+		rear->notes = new char[strlen(trip->notes) + 1];
+		rear->landmarks = new char[strlen(trip->landmarks) + 1];
+
+		strcpy(rear->name, trip->name);
+		strcpy(rear->traffic, trip->traffic);
+		strcpy(rear->notes, trip->notes);
+		strcpy(rear->landmarks, trip->landmarks);
+		rear->length = trip->length;
+
+		return true;
+	}
+	return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return_trip::return_trip()
+{
+	top_index = 0;
+	head = NULL;
+}
+
+return_trip::~return_trip()
+{
+	node* temp;
+	if(head)
+	{
+		while(head->next != NULL)
+		{
+			temp = head;
+			head = head->next;
+
+			delete [] temp->stack;
+			temp->next = NULL;
+		}
+		delete [] head->stack;
+	}
+}
+
+bool return_trip::display()
+{
+	int temp_index = top_index;
+	node* cur;
+
+	if(!head)
+		return false;
+	else
+	{
+		for(cur = head; cur != NULL; cur = cur->next)
+		{
+			while(temp_index != 0)
+			{
+				--temp_index;
+				cout << cur->stack[temp_index].name << endl;
+			}		
+			temp_index = MAX;
+		}
+		return true;
+	}
+	return false;
+}
+
+
+
+
+
+
+trip_section* return_trip::peek()
+{
+	if(!head)
+		return NULL;
+	else
+		return head->stack[top_index-1];
+}
+
+bool return_trip::pop()
+{
+	if(!head)
+		return false;
+	else
+	{
+		if(head->next == NUll && top_index == 0)
+		{
+			top_index = 0;
+			delete head;
+			head = NULL;
+			return true;
+		}
+		else if(top_index == 1)
+		{
+			top_index = MAX;
+			temp = head;
+			head = head->next;
+			delete temp;
+			return true;
+		}
+		else
+		{
+			--top_index;
+			return true;
+		}
+
+	}
+	return false;
+}
+
+
+
+bool return_trip::push(trip_section* trip)
+{
+	node* temp;
+	
+	if(!head)
+	{
+		head = new node;
+		head->next = NULL;
+
+		head->stack = new trip_section[MAX];
+
+		head->stack[top_index].name = new char[strlen(trip->name) + 1];
+		head->stack[top_index].traffic = new char[strlen(trip->traffic) + 1];
+		head->stack[top_index].notes = new char[strlen(trip->notes) + 1];
+		head->stack[top_index].landmarks = new char[strlen(trip->landmarks) + 1];
+
+		strcpy(head->stack[top_index].name, trip->name);
+		strcpy(head->stack[top_index].traffic, trip->traffic);
+		strcpy(head->stack[top_index].notes, trip->notes);
+		strcpy(head->stack[top_index].landmarks, trip->landmarks);
+		head->stack[top_index].length = trip->length;
+
+		++top_index;
+		return true;
+	}		
+	else
+	{
+		if(top_index >= MAX)
+		{
+			top_index = 0;
+			temp = head;
+			head = new node;
+
+			head->next = temp;	
+
+			head->stack = new trip_section[MAX];
+
+			head->stack[top_index].name = new char[strlen(trip->name) + 1];
+			head->stack[top_index].traffic = new char[strlen(trip->traffic) + 1];
+			head->stack[top_index].notes = new char[strlen(trip->notes) + 1];
+			head->stack[top_index].landmarks = new char[strlen(trip->landmarks) + 1];
+
+			strcpy(head->stack[top_index].name, trip->name);
+			strcpy(head->stack[top_index].traffic, trip->traffic);
+			strcpy(head->stack[top_index].notes, trip->notes);
+			strcpy(head->stack[top_index].landmarks, trip->landmarks);
+			head->stack[top_index].length = trip->length;
+
+			++top_index;
+			return true;
+		}
+		else
+		{
+
+			head->stack[top_index].name = new char[strlen(trip->name) + 1];
+			head->stack[top_index].traffic = new char[strlen(trip->traffic) + 1];
+			head->stack[top_index].notes = new char[strlen(trip->notes) + 1];
+			head->stack[top_index].landmarks = new char[strlen(trip->landmarks) + 1];
+
+			strcpy(head->stack[top_index].name, trip->name);
+			strcpy(head->stack[top_index].traffic, trip->traffic);
+			strcpy(head->stack[top_index].notes, trip->notes);
+			strcpy(head->stack[top_index].landmarks, trip->landmarks);
+			head->stack[top_index].length = trip->length;
+
+			++top_index;
+			return true;
+
+		}
+
+	}
+	return false;
+}
