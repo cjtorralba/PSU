8cf040d38401191c914516f703a45d860b7461b1
cjtorralba <cjtorralba@gmail.com>
2020-04-23 06:20:24 -0700

    actual final version

--- Term_Three/prog1/title.h
+++ Term_Three/prog1/title.h
@@ -10,61 +10,69 @@ using namespace std;
 class title
 {
 	public:
+		//copy constructor and defauly, as well as descturcot
 		title(char* name, char* date);
 		title(const title & title);
 		title();	~title();	
 		
-
+	//display function
 		void display();
+
+	//get/setters
 		void changeTitle(char* name, char* date);
 		void changeTitle(const title & toChange);
 		void setTitleName(char* name);
 		void setTitleDate(char* date);
-		void destroy();
 		
 	protected:
-		char* name;
-		char* date;
+		char* name;	//name of title
+		char* date;	//date of title
 };
 
 
 
 
 
-class video : public title
+class video : public title	//video is a title so it conatins its member functions
 {
 
 	public:
+		//constructors and destructor
 		video();	~video();	
 		video(char* name, char* date, char* description, double length);
 		video(const video & toCopy);
 
+		//display function
 		void display();
+
+
+		//functions to change members of the class
 		void changeVideo(double length, char* description);
 		void changeVideo(const video & toChange);
 
 
 	protected:
-		char* description;
-		double length;
+		char* description;	//description of video
+		double length;		//length of video
 };
 
 class liveStream : public video
 {
 	public:
+		//construcotrs and desturcots
 		liveStream();	~liveStream();
 		liveStream(char* name, char* date, char* description, double length, char* instructor, char* topic);
 		liveStream(const liveStream & toCopy);
 
-
+		//change stream and display fucntions
 		void changeStream(const liveStream & toChange);
 		void display();
 
 
 
 	protected:
-		char* instructor;
-		char* topic;
+		char* instructor;	//instructor teaching the liveStream
+		char* topic;		//topic of the liveStream
 
 };
 
